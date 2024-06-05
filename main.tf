2cf6218f04535216a1f211c0985361ab5e4317dd
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-05 09:27:20 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "name" {
+    provider = azurerm.connectivity
+  name = "tg"
+  location = "East US"
+}
+
+resource "azurerm_resource_group" "name1" {
+    provider = azurerm.connectivity
+  name = "tg1"
+  location = "East US"
+}
