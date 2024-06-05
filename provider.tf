f02637f96c27774701e7c3a11b75a9a4e2fe92fe
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-05 11:35:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,22 +12,19 @@ terraform {
 }
 
 provider "azurerm" {
-  skip_provider_registration = true
-  features {}  # Include at least one "features" block
-  client_secret = "GKO8Q~DXeL2mMv0.hjbehClISxd3ZBTmBsVo2c9G"
-  client_id = "ec95cbba-667b-4709-9140-68cc6739bced"
-  tenant_id = "30bf9f37-d550-4878-9494-1041656caf27"
-  subscription_id = "13ba43d9-3859-4c70-9f8d-182debaa038b"
-}
-
-provider "azurerm" {
   alias = "connectivity"
 features {}
   subscription_id = "13ba43d9-3859-4c70-9f8d-182debaa038b"
+  client_secret = "GKO8Q~DXeL2mMv0.hjbehClISxd3ZBTmBsVo2c9G"
+  client_id = "ec95cbba-667b-4709-9140-68cc6739bced"
+  tenant_id = "30bf9f37-d550-4878-9494-1041656caf27"
 }
 
 provider "azurerm" {
   alias = "management"
 features {}
   subscription_id = "5e0ec067-7409-4ede-ae47-ed0926bad2ee"
+  client_secret = "GKO8Q~DXeL2mMv0.hjbehClISxd3ZBTmBsVo2c9G"
+  client_id = "ec95cbba-667b-4709-9140-68cc6739bced"
+  tenant_id = "30bf9f37-d550-4878-9494-1041656caf27"
 }
