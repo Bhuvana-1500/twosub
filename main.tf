0ef4b82401886d1694391b78596b622ed9ba0c7c
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-05 11:17:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "name" {
-    provider = azurerm.connectivity
+  provider = azurerm.connectivity
   name = "tg"
   location = "East US"
 }
 
 resource "azurerm_resource_group" "name1" {
-    provider = azurerm.management
+  provider = azurerm.management
   name = "tg1"
   location = "East US"
 }
